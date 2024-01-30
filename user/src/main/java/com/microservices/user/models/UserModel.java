62b6584f8f2e8d5b3aab229030a3031f33591296
VictorAlvesD <victoralvesrdias@gmail.com>
2024-01-30 11:16:14 -0300

    Crianção UserModel e referencias OneToMany

--- user/src/main/java/com/microservices/user/models/UserModel.java
+++ user/src/main/java/com/microservices/user/models/UserModel.java
@@ -1,15 +1,17 @@
 package com.microservices.user.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.List;
+
 
 @Entity
 @Table(name = "TB_USERS")
 public class UserModel implements Serializable {
+    //Serializable para termos controle de versões
     private static final long UID = 1L;
+
     @Id
     private Long id;
 
@@ -19,11 +21,14 @@ public class UserModel implements Serializable {
 
     private String password;
 
-    private String adress;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AdressModel> addresses;
 
-    private String telephone;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TelephoneModel> telephones;
 
-    private String userCategory;
+    @Enumerated(EnumType.STRING)
+    private UserCategory userCategory;
 
     public void setId(Long id) {
         this.id = id;
@@ -32,6 +37,52 @@ public class UserModel implements Serializable {
     public Long getId() {
         return id;
     }
-    //Serializable para termos controle de versões
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<AdressModel> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<AdressModel> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<TelephoneModel> getTelephones() {
+        return telephones;
+    }
+
+    public void setTelephones(List<TelephoneModel> telephones) {
+        this.telephones = telephones;
+    }
+
+    public UserCategory getUserCategory() {
+        return userCategory;
+    }
+
+    public void setUserCategory(UserCategory userCategory) {
+        this.userCategory = userCategory;
+    }
 }
