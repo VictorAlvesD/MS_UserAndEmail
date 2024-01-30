bf30616ffbd16fea41bddf52b9b9d9a9452bc0c2
VictorAlvesD <victoralvesrdias@gmail.com>
2024-01-30 11:01:40 -0300

    Crianção AdressModel

--- user/src/main/java/com/microservices/user/models/AdressModel.java
+++ user/src/main/java/com/microservices/user/models/AdressModel.java
@@ -1,4 +1,51 @@
 package com.microservices.user.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_Adress")
 public class AdressModel {
+
+    @Id
+    private Long id;
+
+    private String cep;
+
+    private Integer number;
+
+    private String complemet;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getComplemet() {
+        return complemet;
+    }
+
+    public void setComplemet(String complemet) {
+        this.complemet = complemet;
+    }
 }
