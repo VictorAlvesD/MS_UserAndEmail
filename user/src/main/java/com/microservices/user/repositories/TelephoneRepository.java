191a56ccbef09245c1ea9e087732567cb77ed4d1
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-05 09:38:12 -0300

    Corrigindo classe model na declaração generica do JPARepository

--- user/src/main/java/com/microservices/user/repositories/TelephoneRepository.java
+++ user/src/main/java/com/microservices/user/repositories/TelephoneRepository.java
@@ -1,10 +1,11 @@
 package com.microservices.user.repositories;
 
+import com.microservices.user.models.TelephoneModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TelephoneRepository extends JpaRepository<TelephoneRepository, UUID> {
+public interface TelephoneRepository extends JpaRepository<TelephoneModel, UUID> {
 }
