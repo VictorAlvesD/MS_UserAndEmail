13e5fa2eb33a85def8d429e9d6ec55ad641c49c3
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-07 10:52:51 -0300

    Alterando o parametro do listenEmailQueue para receber o EmailDto

--- email/src/main/java/com/microservices/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservices/email/consumers/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.microservices.email.consumers;
 
+import com.microservices.email.dtos.EmailDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,8 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer {
     //Metodo ouvinte, que vai consumir as mensagens da fila
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmailQueue(@Payload String string){
-        System.out.println(string);
+    public void listenEmailQueue(@Payload EmailDto emailDto){
+        System.out.println(emailDto.emailTo());
 
     }
 }
