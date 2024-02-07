e01f0f721d808e15f13a6da398550b6260145ad9
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-07 10:36:59 -0300

    Criando conexão com rabbitMQ na CloudAMQP e configurações

---
+++ email/src/main/java/com/microservices/email/configs/RabbitMQConfig.java
@@ -0,0 +1,20 @@
+package com.microservices.email.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${broker.queue.email.name}")
+    private String queue;
+
+    // Este método cria um bean do tipo Queue, que representa uma fila do RabbitMQ
+    @Bean // Esta anotação indica que este método é um método fábrica que retorna um objeto que será registrado como um bean no contexto do Spring
+    public Queue queue() {
+        return new Queue(queue, true); //
+        // O construtor da classe Queue recebe dois parâmetros: o nome da fila e um booleano que indica se a fila
+        // é durável, ou seja se o broker caí e voltar essa fila vai ser preservada
+    }
+}
